b348803db9eeaad21679856e3e806d6cf340a641
Phakin <phakin.che@gmail.com>
2017-07-12 00:05:21 +0700

    Setup and Configure AWS CLI

---
+++ ubuntu/aws-cli/configure.sh
@@ -0,0 +1,6 @@
+echo "Sample:"
+echo "AWS Access Key ID [None]: AKIAIOSFODNN7EXAMPLE"
+echo "AWS Secret Access Key [None]: wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+echo "Default region name [None]: us-west-2"
+echo "Default output format [None]: json"
+aws configure
