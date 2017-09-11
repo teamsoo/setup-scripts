6ba6c6c52cee3c8858ce697352c18c95820504af
Phakin <phakin.che@gmail.com>
2017-09-11 11:33:26 +0700

    Add modprobe

--- ubuntu/docker/nvidia-docker-install.sh
+++ ubuntu/docker/nvidia-docker-install.sh
@@ -1,2 +1,5 @@
+sudo apt-add-repository multiverse
+sudo apt-get update
+sudo apt install -y nvidia-modprobe
 wget -P /tmp https://github.com/NVIDIA/nvidia-docker/releases/download/v1.0.1/nvidia-docker_1.0.1-1_amd64.deb
 sudo dpkg -i /tmp/nvidia-docker*.deb && rm /tmp/nvidia-docker*.deb
