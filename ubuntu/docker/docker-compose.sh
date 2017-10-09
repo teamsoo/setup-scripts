9d6d0e532ed20bb0b68b4f166ce5e313a8a3e89f
Phakin <phakin.che@gmail.com>
2017-10-09 13:49:38 +0700

    Update docker-compose to 1.16.1

--- ubuntu/docker/docker-compose.sh
+++ ubuntu/docker/docker-compose.sh
@@ -1,3 +1,3 @@
-sudo curl -o /usr/local/bin/docker-compose -L "https://github.com/docker/compose/releases/download/1.15.0/docker-compose-$(uname -s)-$(uname -m)"
+sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose -v
