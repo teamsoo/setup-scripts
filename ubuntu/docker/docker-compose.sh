52cc04e7b8fd9f6a9ae4afbd89c39e00b7993dd9
Phakin <phakin.che@gmail.com>
2017-10-09 13:43:37 +0700

    Add docker-compose

---
+++ ubuntu/docker/docker-compose.sh
@@ -0,0 +1,3 @@
+sudo curl -o /usr/local/bin/docker-compose -L "https://github.com/docker/compose/releases/download/1.15.0/docker-compose-$(uname -s)-$(uname -m)"
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose -v
