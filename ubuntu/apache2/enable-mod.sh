d94509b96eb2f73bda67aa93cfddf21e3ebb9462
Phakin <phakin.che@gmail.com>
2017-10-09 14:55:49 +0700

    Add apache2

---
+++ ubuntu/apache2/enable-mod.sh
@@ -0,0 +1,11 @@
+sudo a2enmod rewrite
+sudo a2enmod ssl
+sudo a2enmod proxy
+sudo a2enmod proxy_http
+sudo a2enmod proxy_ajp
+sudo a2enmod deflate
+sudo a2enmod headers
+sudo a2enmod proxy_balancer
+sudo a2enmod proxy_connect
+sudo a2enmod proxy_html
+sudo systemctl restart apache2
---
+++ ubuntu/apache2/install.sh
@@ -0,0 +1,3 @@
+sudo apt-get update
+sudo apt-get install apache2
+sudo systemctl restart apache2
